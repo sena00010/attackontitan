d9dbe603bb8eed8393662bcdbe6d7529104492bd
SncOne <turkergurel19@gmail.com>
2024-07-15 21:21:34 +0300

    fixed image problem and added storage system

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,11 @@ const nextConfig = {
                 hostname:  "images.rawpixel.com",
                 port: '',
             },
+            {
+                protocol: 'https',
+                hostname:  "firebasestorage.googleapis.com",
+                port: '',
+            },
         ],
     },
 
--- src/component/forumDataFetch/index.tsx
+++ src/component/forumDataFetch/index.tsx
@@ -2,7 +2,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./forum.module.css";
-import { faComment, faHashtag, faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faComment,
+  faHashtag,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import ForumComment from "@/component/forumCommentData";
@@ -10,6 +14,7 @@ import PostCreated from "@/component/createPost";
 import { collection, getDocs, getFirestore } from "@firebase/firestore";
 import { app } from "@/app/layout";
 import UserData from "./userData";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function ForumDataFetch() {
   const [data, setData] = useState<any>([]);
@@ -21,7 +26,10 @@ export default function ForumDataFetch() {
     try {
       const postsCol = collection(db, "post");
       const postSnapshot = await getDocs(postsCol);
-      const postList = postSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      const postList = postSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
       setData(postList ?? []);
       console.log(postList, "postList");
     } catch (error) {
@@ -41,25 +49,44 @@ export default function ForumDataFetch() {
           setClosed={setOpenPost}
           submitFunc={() => {}}
         />
-        {data.map((item: any) => (
-          <div className={styles.post} key={item.id}>
-            <div className={styles.userData}>
-              <UserData userId={item.userId} />
+        {data.map((item: any) => {
+          return (
+            <div className={styles.post} key={item.id}>
+              <div className={styles.userData}>
+                <UserData userId={item.userId} />
+              </div>
+              <div className={styles.postContent}>
+                <div>{item.postContent?.text}</div>
+                {item.postContent?.images && (
+                  <div>
+                    <Swiper
+                      navigation
+                      spaceBetween={10}
+                      slidesPerView={2}
+                      grabCursor={true}
+                      pagination={{ clickable: true }}
+                    >
+                      {item.postContent.images?.map(
+                        (postImage: any, index: any) => {
+                          return (
+                            <SwiperSlide key={index}>
+                              <Image
+                                src={postImage}
+                                alt={`Post image ${index + 1}`}
+                                width={110}
+                                height={140}
+                              />
+                            </SwiperSlide>
+                          );
+                        }
+                      )}
+                    </Swiper>
+                  </div>
+                )}
+              </div>
             </div>
-            <div className={styles.postContent}>
-              <div>{item.postContent?.text}</div>
-              {item.postContent?.image && (
-                <div>
-                  <img
-                    src={item.postContent.image}
-                    alt={item.postContent.image}
-                    className={styles.postImage}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div>
         <button
