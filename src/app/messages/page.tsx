87621881e0cd7903a90ada9d90de433ca1c61dc4
sena Özyiğit <156360194+sena00010@users.noreply.github.com>
2024-08-08 00:12:10 +0300

    message page created

---
+++ src/app/messages/page.tsx
@@ -0,0 +1,10 @@
+import MessagesPart from '@/component/MessagesPart'
+import React from 'react'
+
+const Message = () => {
+  return (
+    <MessagesPart/>
+  )
+}
+
+export default Message
---
+++ src/component/MessagesPart/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MessagesPart = () => {
+  return (
+    <div>mesajlaşma satfası</div>
+  )
+}
+
+export default MessagesPart
